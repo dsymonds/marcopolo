46c1043be08183b407178f9696ce268b2d11ca56
David Symonds <dsymonds@gmail.com>
2008-07-25 19:01:19 +1000

    Extend unit test for ExactRule.

--- Source/ExactRuleTest.m
+++ Source/ExactRuleTest.m
@@ -29,14 +29,22 @@
 	ExactRule *rule = [ExactRule exactRuleWithSensor:@"Mock" value:[NSNumber numberWithInt:1]];
 	STAssertFalse([rule matches:[ValueSet valueSet]], nil);
 
+	// Just that exact value present
 	NSMutableArray *values = [NSMutableArray arrayWithObject:[NSNumber numberWithInt:1]];
 	STAssertTrue([rule matches:[self mockValueSetWithValues:values]], nil);
 
+	// Other values added
 	[values addObject:[NSNumber numberWithInt:2]];
 	[values addObject:[NSNumber numberWithInt:3]];
 	STAssertTrue([rule matches:[self mockValueSetWithValues:values]], nil);
 
+	// Value no longer there
 	[values removeObject:[NSNumber numberWithInt:1]];
+	STAssertFalse([rule matches:[self mockValueSetWithValues:values]], nil);
+
+	// Completely different sensor name, but same value
+	ValueSet *vs = [ValueSet valueSet];
+	[vs setValue:[NSNumber numberWithInt:1] forSensor:@"OtherMock"];
 	STAssertFalse([rule matches:[self mockValueSetWithValues:values]], nil);
 }
 
