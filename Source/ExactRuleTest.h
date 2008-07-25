ee53319e6dcaf29306b837fad6eb502839c4e5ab
David Symonds <dsymonds@gmail.com>
2008-07-25 18:52:37 +1000

    Add a simple unit test for ExactRule class.

---
+++ Source/ExactRuleTest.h
@@ -0,0 +1,14 @@
+//
+//  ExactRuleTest.h
+//  MarcoPolo
+//
+//  Created by David Symonds on 25/07/08.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface ExactRuleTest : SenTestCase {
+}
+
+@end
---
+++ Source/ExactRuleTest.m
@@ -0,0 +1,43 @@
+//
+//  ExactRuleTest.m
+//  MarcoPolo
+//
+//  Created by David Symonds on 25/07/08.
+//
+
+#import "ExactRule.h"
+#import "ExactRuleTest.h"
+#import "ValueSet.h"
+
+
+@implementation ExactRuleTest
+
+- (ValueSet *)mockValueSetWithValues:(NSArray *)valueArray
+{
+	ValueSet *vs = [ValueSet valueSet];
+	[vs setValuesForSensor:@"Mock" values:valueArray];
+	return vs;
+}
+
+- (ValueSet *)mockValueSetWithValue:(NSObject *)value
+{
+	return [self mockValueSetWithValues:[NSArray arrayWithObject:value]];
+}
+
+- (void)testRuleMatching
+{
+	ExactRule *rule = [ExactRule exactRuleWithSensor:@"Mock" value:[NSNumber numberWithInt:1]];
+	STAssertFalse([rule matches:[ValueSet valueSet]], nil);
+
+	NSMutableArray *values = [NSMutableArray arrayWithObject:[NSNumber numberWithInt:1]];
+	STAssertTrue([rule matches:[self mockValueSetWithValues:values]], nil);
+
+	[values addObject:[NSNumber numberWithInt:2]];
+	[values addObject:[NSNumber numberWithInt:3]];
+	STAssertTrue([rule matches:[self mockValueSetWithValues:values]], nil);
+
+	[values removeObject:[NSNumber numberWithInt:1]];
+	STAssertFalse([rule matches:[self mockValueSetWithValues:values]], nil);
+}
+
+@end
